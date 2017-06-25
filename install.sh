f4f10e34e535e15b8cc1b09b347c354d704083d2
pratipk <pratipkar@gmail.com>
2017-06-25 20:03:59 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 # install.sh
-echo "Install script executing......................"
+echo "Install script executing......................" > /tmp/vng.log
 echo "docker-compose -f /home/nav/NAV_demo.yml up -d"
