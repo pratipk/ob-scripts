735e8041e2b621e0cd30a2fcf949fa824f26ca4a
pratipk <pratipkar@gmail.com>
2017-08-09 21:28:53 +0530

    Update server_configure.sh

--- server_configure.sh
+++ server_configure.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "Config script" >> /tmp/vnf.log
-echo "My IP:" $internal_floatingIp >> /tmp/vnf.log
-echo "Server IP:" $server_internal_floatingIp >> /tmp/vnf.log
+echo $internal_floatingIp > my_ip
+echo $server_internal_floatingIp > server_ip
