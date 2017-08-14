1da66a6b0d1ce9ec3b7a0aa40aa504e62ad60c53
pratipk <pratipkar@gmail.com>
2017-08-14 16:25:29 +0530

    Update server_configure.sh

--- server_configure.sh
+++ server_configure.sh
@@ -4,3 +4,9 @@ echo $internal_floatingIp > my_ip
 echo $server_internal_floatingIp > server_ip
 echo "MY_IP="$internal_floatingIp > .env
 echo "SERVER_IP="$server_internal_floatingIp >> .env
+echo "VNFM_IP"=$vnfm_address >> .env
+
+echo "Server="$vnfm_address >> /etc/zabbix/zabbix_agentd.conf
+echo "ServerActive="$vnfm_addres >> /etc/zabbix/zabbix_agentd.conf
+
+service zabbix-agent restart
