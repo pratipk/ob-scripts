b24695c6d5c950fffaf033a1bb4b8d2939efbf48
pratipk <pratipkar@gmail.com>
2017-08-14 21:13:11 +0530

    Update server_configure.sh

--- server_configure.sh
+++ server_configure.sh
@@ -9,6 +9,9 @@ echo "SERVER_IP="$server_internal_floatingIp >> .env
 echo "VNFM_IP="$vnfm_address >> .env
 
 apt-get install -y zabbix-agent
+
+sed -i 's/^Server=\(.*\)/Server=$vnfm_address/' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/^ServerActive=\(.*\)/ServerActive=$vnfm_address/' /etc/zabbix/zabbix_agentd.conf
 echo "Server="$vnfm_address >> /etc/zabbix/zabbix_agentd.conf
 echo "ServerActive="$vnfm_address >> /etc/zabbix/zabbix_agentd.conf
 
