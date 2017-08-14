c80211a75416cacafe7a96b70da7e6490e6d5a2e
pratipk <pratipkar@gmail.com>
2017-08-14 21:01:08 +0530

    Update server_configure.sh

--- server_configure.sh
+++ server_configure.sh
@@ -2,6 +2,8 @@
 echo "Config script" >> /tmp/vnf.log
 echo $internal_floatingIp > my_ip
 echo $server_internal_floatingIp > server_ip
+echo $vnfm_address > vnfm_ip
+
 echo "MY_IP="$internal_floatingIp > .env
 echo "SERVER_IP="$server_internal_floatingIp >> .env
 echo "VNFM_IP="$vnfm_address >> .env
